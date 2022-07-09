5e0b9ab51e4abaef87b0433fcae6180e4be9bbd5
Joseph Tran <fred.smoth@gmail.com>
2022-07-09 13:59:30 +1000

    Got a raw consumer and producer pattern working
    - producers.rs creates messages and then closes itself down
    - consumers.rs is forever listening to messages

---
+++ src/producers.rs
@@ -0,0 +1,48 @@
+use std::time::Duration;
+use log::{info, warn};
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::TopicPartitionList;
+
+mod logging;
+
+async fn produce(server: &str, queue: &str) {
+    let producer: &FutureProducer = &ClientConfig::new()
+        .set("bootstrap.servers", server)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation failure");
+
+    let futures = (0..5)
+        .map(|i| async move {
+            let delivery_status = producer
+                .send(
+                    FutureRecord::to(queue)
+                        .payload(&format!("Message Chezburger{}", i))
+                        .key(&format!("Key {}", i))
+                    // .headers(OwnedHeaders::new().insert(Header {
+                    //     key: "header_key",
+                    //     value: Some("header_value")
+                    // }))
+                    ,
+                    Duration::from_secs(0)
+                )
+                .await;
+            delivery_status
+        })
+        .collect::<Vec<_>>();
+
+    for future in futures {
+        info!("Future completed. Result: {:?}", future.await);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    logging::setup_logger();
+    info!("Hello world");
+    let kafka = "localhost:9092";
+    let queue = "incoming_orders";
+    produce(kafka, queue).await;
+}
